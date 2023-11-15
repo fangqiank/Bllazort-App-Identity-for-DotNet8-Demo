85556a7fb1549ed45d5737285b59abfda820f105
fangqiank <fangqiank@yahoo.com>
2023-11-15 15:39:17 -0700

    Add project files.

---
+++ BllazortAppIdentityDotNet8/BllazortAppIdentityDotNet8.Client/UserInfo.cs
@@ -0,0 +1,9 @@
+namespace BllazortAppIdentityDotNet8.Client
+{
+    public class UserInfo
+    {
+        public required string UserId { get; set; }
+        public required string Email { get; set; }
+        public required string Role { get; set; }
+    }
+}
